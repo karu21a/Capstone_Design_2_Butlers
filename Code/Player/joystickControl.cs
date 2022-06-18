94fca312fc2f731ca058b4dfb556aab6dc416027
김민서 <63498473+karu21a@users.noreply.github.com>
2022-06-18 22:33:51 +0900

    Create joystickControl.cs

---
+++ Code/Player/joystickControl.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class joystickController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private RectTransform lever;
+    private RectTransform rectTransform;
+
+    [SerializeField, Range(10, 150)]
+    private float leverRange;
+
+    private Vector3 inputDirection;
+    private bool isInput;
+
+    [SerializeField]
+    private playerCat.playerMove controller;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+        isInput = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+        controller.Move(Vector2.zero);
+    }
+
+    private void ControlJoystickLever(PointerEventData eventData)
+    {
+        var inputPos = eventData.position - rectTransform.anchoredPosition;
+        var inputVector = inputPos.magnitude <leverRange ? inputPos : inputPos.normalized * leverRange;
+        lever.anchoredPosition = inputVector;
+        inputDirection = inputVector / leverRange;
+    }
+
+    private void InputControlVector()
+    {
+        controller.Move(inputDirection);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInput)
+        {
+            InputControlVector();
+        }
+    }
+}
